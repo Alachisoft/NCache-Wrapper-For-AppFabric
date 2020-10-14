748b922dfb0bb87de800366190ed9b5126574b06
obaid-rehman <obaid@alachisoft.com>
2020-10-14 15:41:43 +0500

    Modified DataCacheNotificationProperties. Equals and GetHashCode methods overridden and internal copy constructor added

--- src/DataCacheNotificationProperties.cs
+++ src/DataCacheNotificationProperties.cs
@@ -9,10 +9,54 @@ namespace Alachisoft.NCache.Data.Caching
     {
         #region[    Constructor ]
         public DataCacheNotificationProperties(long maxQueueLength, TimeSpan pollInterval)
-        { }
+        {
+            MaxQueueLength = maxQueueLength;
+            PollInterval = pollInterval;
+        }
+
+        internal DataCacheNotificationProperties(DataCacheNotificationProperties other)
+        {
+            MaxQueueLength = other.MaxQueueLength;
+            PollInterval = other.PollInterval;
+        }
         #endregion
 
         public long MaxQueueLength { get; private set; }
         public TimeSpan PollInterval { get; private set; }
+
+        public override bool Equals(object obj)
+        {
+            if (this is null)
+            {
+                if (obj is null)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            if (!(this is null) && (obj is null))
+            {
+                return false;
+            }
+
+            var other = obj as DataCacheNotificationProperties;
+
+            if (other is null)
+            {
+                return false;
+            }
+
+            return MaxQueueLength == other.MaxQueueLength && PollInterval == other.PollInterval;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return base.GetHashCode(); 
+            }
+        }
     }
 }
