651b3993920cbfb4a77fa7356ce4338eb9774f31
obaid-rehman <obaid@alachisoft.com>
2020-11-02 13:14:19 +0500

    Modified DataCacheFailureNotificationCallback class

--- src/DataCacheFailureNotificationCallback.cs
+++ src/DataCacheFailureNotificationCallback.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-namespace Alachisoft.NCache.Data.Caching
+﻿namespace Alachisoft.NCache.Data.Caching
 {
     public delegate void DataCacheFailureNotificationCallback(string cacheName, DataCacheNotificationDescriptor nd);
 }
