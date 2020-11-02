0aaeda991e1ef0c5f1e29c997316b7ff0707e6c8
obaid-rehman <obaid@alachisoft.com>
2020-11-02 13:09:54 +0500

    Modified DataCacheProtectionLevel class

--- src/DataCacheProtectionLevel.cs
+++ src/DataCacheProtectionLevel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Alachisoft.NCache.Data.Caching
+﻿namespace Alachisoft.NCache.Data.Caching
 {
     public enum DataCacheProtectionLevel
     {
