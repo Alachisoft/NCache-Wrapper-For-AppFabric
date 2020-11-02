6431e3504b20b69f16139eab6b1eff240bfd3338
obaid-rehman <obaid@alachisoft.com>
2020-11-02 12:58:10 +0500

    Added CacheKey class

---
+++ src/Util/CacheKey.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+
+namespace Alachisoft.NCache.Data.Caching
+{
+    internal class CacheKey
+    {
+        [JsonConstructor]
+        internal CacheKey()
+        {
+
+        }
+
+        public string Key { get; set; }
+
+        public string Region { get; set; }
+
+        public override string ToString()
+        {
+            if (this == null)
+            {
+                return null;
+            }
+
+            return JsonConvert.SerializeObject(this, new JsonSerializerSettings
+            {
+                ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor
+            });
+        }
+
+        internal static CacheKey Deserialize(string cacheKey)
+        {
+            if (string.IsNullOrWhiteSpace(cacheKey))
+            {
+                return null;
+            }
+
+            return JsonConvert.DeserializeObject<CacheKey>(cacheKey, new JsonSerializerSettings
+            {
+                ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor
+            });
+        }
+    }
+}
